5c3acefb4be7f97f410e9407d613e3be0ac5a099
Leon Carnagie <leonfross@gmail.com>
2020-06-30 18:36:17 -0700

    functionality of employee_management js

--- employee_management.js
+++ employee_management.js
@@ -1,57 +1,94 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var http = require('http');
+// var http = require("http");
 
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3002
-  port: 8080,
+  // Your port; if not 8080
+  port: 3306,
 
   // Your username
   user: "root",
 
   // Your password
-  password: "",
+  password: "root",
   database: "employee_managementDB"
 });
 
+
+function inputData() {
+  inquirer
+    .prompt([
+      {
+        name: "department",
+        type: "input",
+        message: "What is the department you would like to input?"
+      },
+      {
+        name: "role",
+        type: "input",
+        message: "What role would you likento query?"
+      },
+      {
+        name: "employee",
+        type: "input",
+        message: "Which employee name would you like to enter?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+}
+
+
+
 connection.connect(function (err) {
   if (err) throw err;
-  trackEmployee();
+  console.log("connected as id " + connection.threadId + "\n");
+  createInfo();
 });
 
-function trackEmployee() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
 
-      ]
-    })
-    .then(function (answer) {
-      switch (answer.action) {
-        case "Find employee by name":
-          trackEmployee();
-          break;
 
-        // case "Find all artists who appear more than once":
-        //   multiSearch();
-        //   break;
+function createInfo() {
+  console.log("Inserting a new Info...\n");
+  var query = connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      first_name: "Rocky",
+      last_name: "Brown",
+      role_id: 23350,
+      manager_id: 00023
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " product inserted!\n");
+      // Call updateEmployee AFTER the INSERT completes
+      updateEmployee();
+    }
+  );
 
-        // case "Find data within a specific range":
-        //   rangeSearch();
-        //   break;
+  // logs the actual query being run
+  console.log(query.sql);
+}
 
-        // case "Search for a specific song":
-        //   songSearch();
-        //   break;
+function updateEmployee() {
+  console.log("Updating updating employee info...\n");
+  var query = connection.query(
+    "UPDATE employee SET ? WHERE first_name = ? AND last_name = ?", [{ role_id: 1}, 'Rocky', 'Brown'], 
+    
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      // deleteProduct();
+    }
+  );
 
-        // case "Find artists with a top song and top album in the same year":
-        //   songAndAlbumSearch();
-        //   break;
-      }
-    });
+  // logs the actual query being run
+  console.log(query.sql);
 }
