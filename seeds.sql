ee79e824122bba20f0280aaff8239b1bf027fe0f
Leon Carnagie <leonfross@gmail.com>
2020-07-04 14:46:25 -0700

    Populate seeds file and add to database

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Mika', 'Brown', 32, 013);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bryan', 'Braham', 22, 113);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Zel', 'Clarke', 54, 223);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Lee', 'White', 11, 987);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Abba', 'Wright', 76, 342);
+
+
+INSERT INTO department (name) VALUES ('Bar');
+INSERT INTO department (name) VALUES ('Restaurant');
+INSERT INTO department (name) VALUES ('Kitchen');
+
+INSERT INTO roles (title, salary, department_id) VALUES ('Treehouse', 850, 42);
+
+
+
+
